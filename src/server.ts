066ff53d699e44a519362bc4afb42d5bb266258d
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2024-11-04 10:06:29 -0400

    update

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,6 @@ var certificate = fs.readFileSync(process.env.SSL_CRT, 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
 //var httpServer = http.createServer(app);
-var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 /*
@@ -36,20 +35,6 @@ const server = app.listen(process.env.PORT, async () => {
   logger.info(`Server started on port: ${process.env.PORT}`);
 });
 */
-
-httpServer.listen(process.env.PORT, async () => {
-  const companies = await Company.findAll();
-  const allPromises: any[] = [];
-  companies.map(async c => {
-    const promise = StartAllWhatsAppsSessions(c.id);
-    allPromises.push(promise);
-  });
-
-  Promise.all(allPromises).then(() => {
-    startQueueProcess();
-  });
-  logger.info(`Server started on port: ${process.env.PORT}`);
-});
 httpsServer.listen(process.env.PORT, async () => {
   const companies = await Company.findAll();
   const allPromises: any[] = [];
@@ -78,5 +63,5 @@ cron.schedule("* * * * *", async () => {
 
 });
 
-initIO(httpServer);
-gracefulShutdown(httpServer);
+initIO(httpsServer);
+gracefulShutdown(httpsServer);
